695d46668a594e9f26fc87c8cfde78b80ed1caf2
Pavel Rojtberg <rojtberg@gmail.com>
2021-08-14 16:21:50 +0200

    13 api docs

---
+++ api/13/navtreedata.js
@@ -0,0 +1,161 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "OGRE", "index.html", [
+    [ "API Reference Start Page", "index.html", [
+      [ "What is this?", "index.html#autotoc_md19", null ],
+      [ "It's so BIG!", "index.html#autotoc_md20", null ],
+      [ "License", "index.html#autotoc_md21", null ]
+    ] ],
+    [ "Manual", "manual.html", "manual" ],
+    [ "Tutorials", "tutorials.html", "tutorials" ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Bibliography", "citelist.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", "namespacemembers_type" ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", "namespacemembers_eval" ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", "functions_type" ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Properties", "functions_prop.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_animation.html",
+"_ogre_animation_state_8h.html#gaad9cc8241f80d9d8fb4f112409de792f",
+"_ogre_common_8h.html#gga79dcd426d291c31072c1ad6f183715d6a8a2b43e30e6d8d590e6853e4f46b8103",
+"_ogre_g_l_e_s2_managed_resource_8h.html",
+"_ogre_im_gui_input_listener_8h.html",
+"_ogre_p_c_z_light_8h.html",
+"_ogre_plugin_8h.html",
+"_ogre_render_system_capabilities_8h.html#a1f2980065f3c74646d1c1710f55f3a22",
+"_ogre_script_compiler_8h.html#ggafb79f4c215a4ac77110efe823e94b61ea55a00540fc8557689342cef62742e29a",
+"_ogre_shader_prerequisites_8h.html#a8c100ce939d23573f062f3ad579998a1a08c0d76a7a8df2b6a839749c5be6c1af",
+"_ogre_vulkan_mappings_8h.html",
+"class_ogre_1_1_absolute_pixel_count_lod_strategy.html#ab89c1d47c5f906f91a5f1753db21070b",
+"class_ogre_1_1_anti_portal_factory.html#a2174deb17522078482d2c7dec12c2ecb",
+"class_ogre_1_1_axis_aligned_box.html#a4e8c385234da6a08d8db3371355b9e5f",
+"class_ogre_1_1_bitwise.html#a0b1d028f1096f238ee55edeab8ae2ddb",
+"class_ogre_1_1_camera.html#abb719d4eb036a88cef5e23f7a5579b9c",
+"class_ogre_1_1_colour_value.html#aae078d77b654f76e3b760174f93474ec",
+"class_ogre_1_1_compositor_instance.html#a4e8600e4aaf6b43567a4590d2132bf5d",
+"class_ogre_1_1_d3_d11_device.html#ab6fb0231f935116e97988b6d207941c6",
+"class_ogre_1_1_d3_d11_render_texture.html#ad028e211ccd347372404245580d92d1e",
+"class_ogre_1_1_d3_d9_hardware_buffer.html",
+"class_ogre_1_1_d3_d9_resource.html#a9008da922e8b8762ac284cc1f08a0749",
+"class_ogre_1_1_deflate_stream.html#a7ab66addcbf428322b1caea1d9c88cea",
+"class_ogre_1_1_entity.html#aa4f06ee1cf49b172d482d805441c1e9c",
+"class_ogre_1_1_frustum.html#a1d3edfdc7373308e2cda9af6b618e4ff",
+"class_ogre_1_1_g_l3_plus_texture_buffer.html#a8971a42414b393dc9bb16b7e808a7d36",
+"class_ogre_1_1_g_l_gpu_nvparse_program.html#aead1cb17fd23bb7edb215c431c117e34",
+"class_ogre_1_1_g_l_s_l_1_1_g_l_s_l_program_factory.html#a58d75a505e370202c3d6aa1e744d45b4",
+"class_ogre_1_1_gpu_program.html#a2443ed18b880eb939123bb8daf9165c7",
+"class_ogre_1_1_gpu_program_parameters.html#a3f34a7706fda7853a3e7a2d6f1351f9c",
+"class_ogre_1_1_hardware_buffer_manager_base.html#abe3ea209174c3a40286dde425addb814",
+"class_ogre_1_1_intersection_scene_query.html#a1092da71d6f78d6ecee11eee06a3b1e5",
+"class_ogre_1_1_lod_output_provider_mesh.html#a570655d1d1b34e2853b044e8cb0d95f7",
+"class_ogre_1_1_material.html#abe6a4371ce72c7bf1cf5a0c2e9259815",
+"class_ogre_1_1_memory_data_stream.html#accd65733ea1317c49c5fdf7263ff4cc7",
+"class_ogre_1_1_metal_program.html#ac19055f6d94e185a0a13e8719ff4fa8b",
+"class_ogre_1_1_node.html#a264df23a0763ef9f8879c97bcbba0f47",
+"class_ogre_1_1_octree_scene_manager.html#a5403cb0576366a25b5bcd255e563c01a",
+"class_ogre_1_1_overlay_manager.html#a1d38898d1d08e945f186e2043444588d",
+"class_ogre_1_1_p_c_zone.html",
+"class_ogre_1_1_paged_world.html#a587e49570767c3f5f4e9ecf168ec58be",
+"class_ogre_1_1_particle_system.html#a8d24a0601115d991c92a8c93c28e01a3",
+"class_ogre_1_1_pass.html#abce35807758f26b84d29313cf70593cd",
+"class_ogre_1_1_portal.html#a0782b90dedb9f30a0c908262af9bd184",
+"class_ogre_1_1_quake3_level.html#a76ef64eadd5efca15be9c437242bbb2a",
+"class_ogre_1_1_r_t_shader_1_1_hardware_skinning.html#a1e08469498f970bd5bbca97ebad06fdb",
+"class_ogre_1_1_r_t_shader_1_1_parameter.html#a4fa6286f097d19e92a8ed91645fbfbe7abfb8a34699488433a873da92472f140e",
+"class_ogre_1_1_r_t_shader_1_1_sub_render_state_accessor.html#a7268047892571c6bced796d04fbb4cb3",
+"class_ogre_1_1_render_queue_group.html#a33650a9641d40269f18d44be60006d31",
+"class_ogre_1_1_render_system_capabilities_serializer.html#ad8a0e4b66877c0893a70924dc55bc130",
+"class_ogre_1_1_resource_group_manager.html#a04e9d0f79805a91f1cf292d0feaa8322",
+"class_ogre_1_1_root.html#adc677530cc7ef50fbd0e37d75c02e16b",
+"class_ogre_1_1_scene_manager.html#a66951894e88fd1ebc1876d762700e427",
+"class_ogre_1_1_scene_node.html#a064b81e279b10a488ba018d1ac87abb5",
+"class_ogre_1_1_simple_spline.html#aef641d97c1f208faf7bcf46584706fd8",
+"class_ogre_1_1_static_geometry_1_1_l_o_d_bucket.html#a3f360a28a8a1a27e0baf86142be8ffe0",
+"class_ogre_1_1_sub_entity.html#aa53483999fb9682daba07b25f43e1f7a",
+"class_ogre_1_1_terrain.html#a4a968c60ac228ab79b88e17547b57243",
+"class_ogre_1_1_terrain_group.html#a258a6c8f9655fc48fd4afbfaf9f650dd",
+"class_ogre_1_1_terrain_quad_tree_node.html#ad3f99f3c71c1bff1d323007020f813d5",
+"class_ogre_1_1_texture_unit_state.html#a7b583996c99d3495c02565f0f8863727",
+"class_ogre_1_1_vector.html#a898d6f71ca95d09f784a97ee7129cf46",
+"class_ogre_1_1_volume_1_1_c_s_g_noise_source.html#a19c66b77e26768752590a3ae60cbd592",
+"class_ogre_1_1_vulkan_queue.html#a467d51923bda10ff87b144d4327da925ac44b72f90a5b05fcb30d52ff91139367",
+"class_ogre_1_1_work_queue_1_1_request.html#a787e9f136393d10910474fbb433bf05a",
+"class_ogre_bites_1_1_tray_manager.html#a04b439efaf2a801da5937c9a57dd1387",
+"functions_type_i.html",
+"group___g_l_e_s2.html#gab2d4e98b8546d512af93891c7094b717",
+"group___image.html#gga7e0353e7d36d4c2e8468641b7303d39ca3da45a080dde7e47bb0629d24aade867",
+"group___materials.html#gga9c5b2950be06ff56a6ee0bace240d447a33f4e8ee7271ce5353aa53c04c18447d",
+"group___paging.html#ga0df4533060e2b0a3bc640ca64ab3aa38",
+"group___r_t_shader.html#gga4c92eba13eb8038d35530610c58ceff4a028cd01c8dfcc08f10f9b3de20d1c604",
+"group___resources.html#gaac3431531c3258e3c1c86ef8a4c06228",
+"group___scene.html#gab0b51a9927616fef936e30d5fbae1883",
+"group___script.html#ggafb79f4c215a4ac77110efe823e94b61ea4e04055e24616debe03189c4c92ba4e2",
+"group__deprecated.html#gaa4b6dbd7bf722582a21fcc25d25f73ed",
+"struct_ogre_1_1_concrete_node.html#a6b1a9325b60739ca145616a86e072e5e",
+"struct_ogre_1_1_lod_data_1_1_triangle.html#aff1395c324d8808f43dcc76b8a5cdb3b",
+"struct_ogre_1_1_scene_manager_1_1light_less.html#add266e423661c3b75fffbd3377c6056b",
+"struct_ogre_1_1_vector_base_3_013_00_01_real_01_4.html#a683a3ae0ddceb4a01a94c0727ca63362",
+"struct_ogre_bites_1_1_input_listener.html#a4534ef767309188cee16e4017054cccd"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
