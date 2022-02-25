915de45773fa70af3701ebb88f72d05b47edde03
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-02-25 17:23:26 +0100

    Use libc++ with Clang, enable building googletest with libc++ to resolve link errors

--- CMake/toolchain/linux.toolchain.clang.cmake
+++ CMake/toolchain/linux.toolchain.clang.cmake
@@ -5,6 +5,6 @@ find_program(CMAKE_C_COMPILER clang-13 REQUIRED)
 find_program(CMAKE_CXX_COMPILER clang++-13 REQUIRED)
 
 #do not use libc++ for C objects
-#add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
-#add_link_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+add_link_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
 add_link_options(-fuse-ld=lld)
