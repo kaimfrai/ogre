ef906a9727f442918862321924bf919014a87d2e
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-02-25 13:35:53 +0100

    Add toolchain file for Clang under Linux

---
+++ CMake/toolchain/linux.toolchain.clang.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
+
+find_program(CMAKE_C_COMPILER clang-13 REQUIRED)
+find_program(CMAKE_CXX_COMPILER clang++-13 REQUIRED)
+
+#do not use libc++ for C objects
+#add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+#add_link_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+add_link_options(-fuse-ld=lld)
