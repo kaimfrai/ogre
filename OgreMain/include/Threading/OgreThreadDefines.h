cdeb98330047c5ad15d847f9bb190ab798fc4699
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-03-02 13:49:17 +0100

    Remove deprecated thread providers

--- OgreMain/include/Threading/OgreThreadDefines.h
+++ OgreMain/include/Threading/OgreThreadDefines.h
@@ -27,17 +27,8 @@ THE SOFTWARE
 #define __OgreThreadDefines_H__
 
 #define OGRE_AUTO_MUTEX_NAME mutex
-#if OGRE_THREAD_PROVIDER == 0
-    #include "OgreThreadDefinesNone.h"
-#elif OGRE_THREAD_PROVIDER == 1
-    #include "OgreThreadDefinesBoost.h"
-#elif OGRE_THREAD_PROVIDER == 2
-    #include "OgreThreadDefinesPoco.h"
-#elif OGRE_THREAD_PROVIDER == 3
-    #include "OgreThreadDefinesTBB.h"
-#elif OGRE_THREAD_PROVIDER == 4
-   #include "OgreThreadDefinesSTD.h"
-#endif
+
+#include "OgreThreadDefinesSTD.h"
 
 #if OGRE_THREAD_SUPPORT == 3
     #include "OgreThreadDefinesNone.h"
