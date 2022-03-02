b6a1a4637a8e4cbfe4319fec7caf6d84900319d1
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-03-02 16:53:16 +0100

    Add compile option _LARGEFILE64_SOURCE on Linux as suggested by miniz.h

--- CMake/toolchain/linux.toolchain.gcc.cmake
+++ CMake/toolchain/linux.toolchain.gcc.cmake
@@ -3,3 +3,8 @@ set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
 
 find_program(CMAKE_C_COMPILER gcc-11 REQUIRED)
 find_program(CMAKE_CXX_COMPILER g++-11 REQUIRED)
+
+#from miniz.h:
+# On platforms using glibc, Be sure to "#define _LARGEFILE64_SOURCE 1" before
+# including miniz.c to ensure miniz uses the 64-bit variants
+add_compile_options(-D_LARGEFILE64_SOURCE=1)
