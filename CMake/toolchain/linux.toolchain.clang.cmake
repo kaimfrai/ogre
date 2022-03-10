aac7af9011fdeea29cec0a5dad9a0f676c6162b9
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-03-10 18:51:40 +0100

    Set clang version to 15

--- CMake/toolchain/linux.toolchain.clang.cmake
+++ CMake/toolchain/linux.toolchain.clang.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
 
-find_program(CMAKE_C_COMPILER clang-13 REQUIRED)
-find_program(CMAKE_CXX_COMPILER clang++-13 REQUIRED)
+find_program(CMAKE_C_COMPILER clang-15 REQUIRED)
+find_program(CMAKE_CXX_COMPILER clang++-15 REQUIRED)
 
 #do not use libc++ for C objects
 add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
