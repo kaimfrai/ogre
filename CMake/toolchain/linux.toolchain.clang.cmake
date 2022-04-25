017b98d3616699b1b9064b3f6f849714be5e84a7
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-04-25 17:55:39 +0200

    ABI breakage with gtest

--- CMake/toolchain/linux.toolchain.clang.cmake
+++ CMake/toolchain/linux.toolchain.clang.cmake
@@ -1,10 +1,13 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ${CMAKE_HOST_SYSTEM_PROCESSOR})
 
-find_program(CMAKE_C_COMPILER clang-13 REQUIRED)
-find_program(CMAKE_CXX_COMPILER clang++-13 REQUIRED)
+find_program(CMAKE_C_COMPILER clang-15 REQUIRED)
+find_program(CMAKE_CXX_COMPILER clang++-15 REQUIRED)
 
 #do not use libc++ for C objects
-#add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
-#add_link_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
-add_link_options(-fuse-ld=lld)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+add_link_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++>)
+add_link_options($<$<COMPILE_LANGUAGE:CXX>:-lm>)
+add_link_options($<$<COMPILE_LANGUAGE:CXX>:-lc++>)
+add_link_options($<$<COMPILE_LANGUAGE:CXX>:-lc++abi>)
+#add_link_options(-fuse-ld=lld)
