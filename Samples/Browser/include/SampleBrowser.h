26999526c270afb69aa49176c8585209fbd659c1
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-05-04 17:28:50 +0200

    [Memory] Remove loading bar
    Loading bar used inconsistent amounts of memory

--- Samples/Browser/include/SampleBrowser.h
+++ Samples/Browser/include/SampleBrowser.h
@@ -744,9 +744,7 @@ namespace OgreBites
 
             createDummyScene();
 
-            mTrayMgr->showLoadingBar(1, 0);
             Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
-            mTrayMgr->hideLoadingBar();
         }
 
         /*-----------------------------------------------------------------------------
