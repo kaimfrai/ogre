769f3ba2a79ef5ce133054db4c0806b8bad1cab4
kmuell23 <57592364+kmuell23@users.noreply.github.com>
2022-05-05 13:09:55 +0200

    Revert "[Memory] Remove Statspanel from Tray"
    This reverts commit 26af9f38d8c396c35e9f465782370e4f37d2f2a6.

--- Components/Bites/include/OgreTrays.h
+++ Components/Bites/include/OgreTrays.h
@@ -908,6 +908,24 @@ namespace OgreBites
         void showFrameStats(TrayLocation trayLoc, size_t place = -1);
 
         /**
+        Hides frame statistics widget set.
+        */
+        void hideFrameStats();
+
+        bool areFrameStatsVisible()
+        {
+            return mFpsLabel != 0;
+        }
+
+        /**
+        Toggles visibility of advanced statistics.
+        */
+        void toggleAdvancedFrameStats()
+        {
+            if (mFpsLabel) labelHit(mFpsLabel);
+        }
+
+        /**
         Shows logo in the specified location.
         */
         void showLogo(TrayLocation trayLoc, size_t place = -1);
@@ -1161,6 +1179,8 @@ namespace OgreBites
         Button* mYes;                         // top priority Yes button
         Button* mNo;                          // top priority No button
         bool mCursorWasVisible;               // cursor state before showing dialog
+        Label* mFpsLabel;                     // FPS label
+        ParamsPanel* mStatsPanel;             // frame stats panel
         DecorWidget* mLogo;                   // logo
         Ogre::Real mGroupInitProportion;      // proportion of load job assigned to initialising one resource group
         Ogre::Real mGroupLoadProportion;      // proportion of load job assigned to loading one resource group
